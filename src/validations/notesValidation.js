6ccb8a3b3b4bfdcabd573103445d11c03d459bb7
Ihor Shuduria <igorshudurya@gmail.com>
2025-12-18 16:31:40 +0100

    fix: correct query validation for get all notes

--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -3,7 +3,7 @@ import { TAGS } from '../constants/tags.js';
 import { isValidObjectId } from 'mongoose';
 
 export const getAllNotesSchema = {
-  [Segments.PARAMS]: Joi.object({
+  [Segments.QUERY]: Joi.object({
     page: Joi.number().integer().min(1).default(1),
     perPage: Joi.number().integer().min(5).max(20).default(10),
     tag: Joi.string().valid(...TAGS),
